66f183b9226183e999e95c307d7681d8a8e09b6a
Rebecca W Silverstein <rswhipple@gmail.com>
2024-02-09 12:43:17 -0800

    CHORE: replace generic element with t_block in hash_table

--- hash_table.c
+++ hash_table.c
@@ -1,41 +1,34 @@
 #include "hash_table.h"
-#include <stdio.h>
-#include <string.h>
-
-struct entry {
-    char *key;
-    void *object;
-    struct entry *next;
-};
+#include "my_malloc.h"
 
 struct hash_table_s {
     uint32_t size;
     hash_function *hash;
-    entry **elements;
+    t_block **elements;
 };
 
-static size_t hash_table_index(hash_table *ht, const char *key) {
+static size_t hash_table_index(t_hash *ht, const char *key) {
     size_t result = (ht->hash(key, strlen(key)) % ht->size);
     return result;
 }
 
-hash_table *hash_table_create(uint32_t size, hash_function *hf) {
-    hash_table *ht = malloc(sizeof(*ht));
+t_hash *hash_table_create(t_heap *heap, uint32_t size, hash_function *hf) {
+    t_hash *ht = HASH_SHIFT(heap);;
     ht->size = size;
     ht->hash = hf;
-    // note: calloc seroes out the memory, which is why we use it
-    ht->elements = calloc(sizeof(entry*), ht->size);
+    // TODO: check if this works??
+    ht->elements = BLOCK_SHIFT(heap) * ht->size;
     return ht;
 }
 
-void hash_table_destroy(hash_table *ht) {
+void hash_table_destroy(t_hash *ht) {
     // cleanup individual elements
     for (uint32_t i = 0; i < ht->size; i++) {
         while (ht->elements[i]) {
-            entry *tmp = ht->elements[i];
+            t_block *tmp = ht->elements[i];
             ht->elements[i] = ht->elements[i]->next;
             free(tmp->key);
-            free(tmp->object); // make more general
+            // free(tmp->object); // TODO: make it work for t_block
             free(tmp);
         }
     }
@@ -43,59 +36,67 @@ void hash_table_destroy(hash_table *ht) {
     free(ht);
 }
 
-void hash_table_print(hash_table *ht) {
+void hash_table_print(t_hash *ht) {
     printf("Start Table\n");
     for (int i = 0; i < ht->size; i++) {
         if (ht->elements[i] == NULL) {
             printf("\t%i\t---\n", i);
         } else {
             printf("\t%i\t\n", i);
-            entry *tmp = ht->elements[i];
+            t_block *tmp = ht->elements[i];
             while (tmp) {
-                printf("\"%s\"(%p) - ", tmp->key, tmp->object);
+                printf("\"%s\"(%lu) - ", tmp->key, tmp->data_size);
                 tmp = tmp->next;
             }
         }
     }
 }
 
-bool hash_table_insert(hash_table *ht, const char *key, void *value) {
+bool hash_table_insert(t_heap **heap, t_hash *ht, const char *key, size_t size) {
     if (key == NULL || value == NULL) return false;
     size_t index = hash_table_index(ht, key);
 
     if (hash_table_allocate(ht, key)) return false;
 
     // create new entry
-    entry *e = malloc(sizeof(entry));
-    e->object = value;
-    e->key = malloc(strlen(key) + 1);
-    strcpy(e->key, key);
-    // can replace last 2 lines with strdup(key)
+    t_block *b = create_block();
+    printf("block: %zu\n", block->data_size);
+    block->prev = NULL;
+    block->next = NULL;
+    block->data_size = size;
+    block->freed = false;
+    (*heap)->block_count++;
+    (*heap)->free_size -= size;
+
+    // TODO fix key and malloc
+    b->key = malloc(strlen(key) + 1);
+    strcpy(b->key, key);
 
     // insert entry
-    e->next = ht->elements[index];
-    ht->elements[index] = e;
+    ht->elements[index]->prev = b;
+    b->next = ht->elements[index];
+    ht->elements[index] = b;
     return true;
 }
 
-void *hash_table_allocate(hash_table *ht, const char *key) {
+void *hash_table_allocate(t_hash *ht, const char *key) {
     if (key == NULL || ht == NULL) return false;
     size_t index = hash_table_index(ht, key);
 
-    entry *tmp = ht->elements[index];
+    t_block *tmp = ht->elements[index];
     while (tmp && strcmp(tmp->key, key) != 0) {
         tmp = tmp->next;
     }
     if (!tmp) return NULL;
-    return tmp->object;
+    return tmp->data_size;
 }
 
-void *hash_table_deallocate(hash_table *ht, const char *key) {
+void *hash_table_deallocate(t_hash *ht, const char *key) {
     if (key == NULL || ht == NULL) return false;
     size_t index = hash_table_index(ht, key);
 
-    entry *tmp = ht->elements[index];
-    entry *prev = NULL;
+    t_block *tmp = ht->elements[index];
+    t_block *prev = NULL;
     while (tmp && strcmp(tmp->key, key) != 0) {
         prev = tmp;
         tmp = tmp->next;
@@ -109,14 +110,14 @@ void *hash_table_deallocate(hash_table *ht, const char *key) {
         // deallocating from the middle 
         prev->next = tmp->next;
     }
-    void *result = tmp->object;
+    void *result = tmp->data_size;
     free(tmp);
 
     return result;
 }
 
 
-unsigned int hashFunction(int key, int tableSize) {
+unsigned int my_hash_function(int key, int tableSize) {
     double A = (sqrt(5) - 1) / 2; // Fractional part of the golden ratio
     return (unsigned int)(tableSize * (key * A - (int)(key * A)));
 }
