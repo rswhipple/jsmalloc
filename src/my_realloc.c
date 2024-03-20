3a724bf27cf9d163293433eaa8edfbf700d00275
Mathius <info@yogimathius.dev>
2024-03-19 18:14:15 -0700

    feat: add untested realloc implementation

--- src/my_realloc.c
+++ src/my_realloc.c
@@ -4,5 +4,22 @@
 void *my_realloc(void *ptr, size_t size) {
     UNUSED(ptr);
     UNUSED(size);
+
+    if (ptr == NULL && size != 0) {
+        return my_malloc(size);
+    }
+
+    if (size == 0) {
+        my_free(ptr);
+        return NULL;
+    }
+
+    void *new_ptr = my_malloc(size);
+
+    if (new_ptr != NULL) {
+        my_free(ptr);
+        return new_ptr;
+    }
+
     return NULL;
 };
