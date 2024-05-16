c9235ae12083f38203109969e8174aaa032db32f
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-16 11:28:07 -0400

    restructure: new datatypes and macros

--- inc/my_malloc.h
+++ inc/my_malloc.h
@@ -14,54 +14,74 @@
 
 #define UNUSED(x) (void)(x)
 
-// #include "hash_table.h"
+typedef struct s_pagemap t_pagemap;
+typedef struct s_span t_span;
+typedef struct s_page t_page;
+typedef struct s_chunk t_chunk;
+
+struct s_pagemap {
+    t_span *span_head;
+    u_int16_t total_pages;
+};
+
+struct s_span {
+    t_span *next;   // 8 bytes (assuming x64)
+    u_int16_t num_pages;    // 2 bytes
+    bool pages_returned;
+    t_page *page_head;
+};
+
+struct s_page {
+    t_page *next;   // 8 bytes 
+    t_page *prev;   // 8 bytes
+    u_int8_t chunk_count;   // 1 byte
+    t_chunk *top_chunk;
+};
+
+struct s_chunk {
+    u_int16_t size;
+    bool in_use;
+    t_chunk* prev;
+    t_chunk* next;
+    void* data;     // figure out how to write over the pointers while in use
+    u_int16_t chunk_size;
+};
+
 typedef struct hash_table_s t_hash;
 typedef unsigned int (hash_function)(size_t input, uint32_t);
-
-typedef struct s_heap {
-    struct s_heap* prev;
-    struct s_heap* next;
-    size_t total_size;
-    size_t free_size;
-    int block_count;
-    t_hash* ht;
-} t_heap;
-
-typedef struct s_block {
-    struct s_block* prev;
-    struct s_block* next;
-    size_t data_size;
-    bool freed;
-    void* object;
-} t_block;
 
 struct hash_table_s {
     uint32_t size;
     hash_function* hash;
-    t_block** elements;
+    t_chunk** elements;
 };
 
-// Macros that return the address of the block and heap
+// Macros that return the address of datatypes within the heap
 // based on the start of the heap
-#define HEAP_SHIFT(start)((char *)start + sizeof(t_heap))
-#define BLOCK_SHIFT(start)((char *)start + sizeof(t_block))
-#define HASH_SHIFT(start)((t_heap *)start + sizeof(t_hash))
-#define MEMORY_SHIFT(start, data_size)((char *)start + data_size)
+#define PAGEMAP_SHIFT(start)((u_int8_t *)start + sizeof(t_pagemap))
+#define SPAN_SHIFT(start)((u_int8_t *)start + sizeof(t_span))
+#define PAGE_SHIFT(start)((u_int8_t *)start + sizeof(t_page))
+#define CHUNK_SHIFT(start)((u_int8_t *)start + sizeof(t_chunk))
+#define HASH_SHIFT(start)((u_int8_t *)start + sizeof(t_hash))
+#define MEMORY_SHIFT(start, data_size)((u_int8_t *)start + data_size)
 
-#define TINY_HEAP_ALLOCATION_SIZE (4 * getpagesize())
-#define TINY_BLOCK_SIZE (TINY_HEAP_ALLOCATION_SIZE / 128)
-#define SMALL_HEAP_ALLOCATION_SIZE (16 * getpagesize())
-#define SMALL_BLOCK_SIZE (SMALL_HEAP_ALLOCATION_SIZE / 128)
+#define PAGE_SIZE getpagesize()
+#define BASE_HEAP_SIZE (48 * PAGE_SIZE)
+#define FAST_PAGE_ALLOCATION_SIZE (8 * PAGE_SIZE)
+#define FAST_PAGE_MAX_CHUNK_SIZE 64
+#define SMALL_HEAP_ALLOCATION_SIZE (20 * PAGE_SIZE)
+#define SMALL_PAGE_MAX_CHUNK_SIZE 512
+#define LARGE_HEAP_ALLOCATION_SIZE (20 * PAGE_SIZE)
+#define LARGE_PAGE_MAX_CHUNK_SIZE (LARGE_HEAP_ALLOCATION_SIZE / 12)
 
-#define MAX_BLOCKS 20   // TODO: 20 is an arbitrary size 
 
-extern t_heap* global_heap;
+// extern t_pagemap* global_heap;
 
 void* my_malloc(size_t size);
 void my_free(void* ptr);
 void* my_realloc(void* ptr, size_t size);
 void* my_calloc(size_t nitems, size_t size);
-void* create_block(t_heap* heap, size_t size);
-void print_blocks(t_heap* heap);
+// void* create_block(t_heap* heap, size_t size);
+// void print_blocks(t_heap* heap);
 
 #endif
