98154c08de2129985bae6808692ecfff22c2a9ec
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-16 15:33:57 -0400

    fix: substitute size_t for u_int16_t in structs for reliability/flexibility

--- inc/my_malloc.h
+++ inc/my_malloc.h
@@ -21,37 +21,37 @@ typedef struct s_chunk t_chunk;
 
 struct s_pagemap {
     t_span *span_head;
-    u_int16_t total_pages;
+    size_t total_pages;
 };
 
 struct s_span {
-    t_span *next;   // 8 bytes (assuming x64)
-    u_int16_t num_pages;    // 2 bytes
-    bool pages_returned;
+    t_span *next;   
     t_page *page_head;
+    size_t num_pages;    
+    bool pages_returned;
 };
 
 struct s_page {
-    t_page *next;   // 8 bytes 
-    t_page *prev;   // 8 bytes
-    u_int8_t chunk_count;   // 1 byte
-    t_chunk *top_chunk;
+    t_page *next;  
+    t_page *prev;   
+    size_t memory;       
+    size_t chunk_count;   
+    t_chunk *top_chunk;     
 };
 
 struct s_chunk {
-    u_int16_t size;
-    bool in_use;
+    size_t size;
     t_chunk* prev;
     t_chunk* next;
     void* data;     // figure out how to write over the pointers while in use
-    u_int16_t chunk_size;
+    size_t chunk_size;
 };
 
 typedef struct hash_table_s t_hash;
 typedef unsigned int (hash_function)(size_t input, uint32_t);
 
 struct hash_table_s {
-    uint32_t size;
+    size_t size;
     hash_function* hash;
     t_chunk** elements;
 };
@@ -75,7 +75,7 @@ struct hash_table_s {
 #define LARGE_PAGE_MAX_CHUNK_SIZE (LARGE_HEAP_ALLOCATION_SIZE / 12)
 
 
-// extern t_pagemap* global_heap;
+extern size_t min_chunk_size;
 
 void* my_malloc(size_t size);
 void my_free(void* ptr);
