432ee5b2fab4f62d0099f225914f66dd60893a44
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-16 11:34:27 -0400

    refactor: simplify main

--- src/main.c
+++ src/main.c
@@ -1,18 +1,12 @@
 #include "../inc/hash_table.h" 
-#include "../inc/heap.h" 
+#include "../inc/pagemap.h" 
 #include "../inc/my_malloc.h"
 
 int main() {
 
-    // For hash table testing
-    const int tablesize = (1<<12);
-    // initiate heap first
-    t_heap *heap = NULL;
-    int heap_size = 1000000;
-
-    create_heap(&heap, heap_size);
-    hash_table_create(heap, tablesize, my_hash_function);
-
+    // initiate memory span and add to pagemap
+    t_pagemap *pagemap = NULL;
+    create_pagemap(&pagemap);
 
     log_info("allocating");
     void *ptr1 = my_malloc(10);
@@ -22,19 +16,14 @@ int main() {
     
     const void *ptr4 = my_malloc(900);
     UNUSED(ptr4);
-    UNUSED(ptr5);
-    t_heap *current_heap = global_heap;
-    while (current_heap != NULL) {
-        log_info("pointers allocated");
-        printf("heap total: %zu\n", current_heap->total_size);
-        printf("remaining size: %zu\n", current_heap->free_size);
-        print_blocks(current_heap);
-        current_heap = current_heap->next;
-    }
 
     log_info("freeing");
     my_free(ptr1);
     my_free(ptr2);
     my_free(ptr3);
+    my_free(ptr4);
+
+    destroy_pageheap(pagemap);
+
     return 0;
 }
