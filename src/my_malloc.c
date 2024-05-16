cf3057079be9c2ea9d7331c97dbece3bf97b46ee
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-16 15:35:15 -0400

    temp: comment out

--- src/my_malloc.c
+++ src/my_malloc.c
@@ -1,40 +1,39 @@
 
 #include "../inc/my_malloc.h" 
 #include "../inc/hash_table.h" 
-#include "../inc/heap.h" 
-
-t_heap *global_heap = NULL;
 
 void *my_malloc(size_t size) {
-    // Still need to handle LARGE_HEAP_ALLOCATION_SIZE
-    int allocation_size = size < (size_t)TINY_BLOCK_SIZE ? TINY_HEAP_ALLOCATION_SIZE : SMALL_HEAP_ALLOCATION_SIZE;
-    printf("allocation size: %d\n", allocation_size);
-    if (global_heap == NULL) {
-        create_heap(&global_heap, allocation_size);
-        return create_block(global_heap, size);
-    } else {
-        t_heap *current_heap = global_heap;
-        while (current_heap != NULL) {
-            if (current_heap->total_size == (size_t)allocation_size && current_heap->free_size >= size) {
-                return create_block(current_heap, size);
-            } 
-            current_heap = current_heap->next;
-        }
+    // initiate pageheap
 
-        t_heap *new_heap = NULL;
-        create_heap(&new_heap, allocation_size);
-        // Add the new heap to the end of the heap list
-        t_heap *last_heap = global_heap;
-        while (last_heap->next != NULL) {
-            last_heap = last_heap->next;
-        }
-        last_heap->next = new_heap;
-        // Return the newly allocated block
-        return create_block(new_heap, size);
-    }
+    // // Still need to handle LARGE_HEAP_ALLOCATION_SIZE
+    // int allocation_size = size < (size_t)TINY_BLOCK_SIZE ? TINY_HEAP_ALLOCATION_SIZE : SMALL_HEAP_ALLOCATION_SIZE;
+    // printf("allocation size: %d\n", allocation_size);
+    // if (global_heap == NULL) {
+    //     create_heap(&global_heap, allocation_size);
+    //     return create_block(global_heap, size);
+    // } else {
+    //     t_heap *current_heap = global_heap;
+    //     while (current_heap != NULL) {
+    //         if (current_heap->total_size == (size_t)allocation_size && current_heap->free_size >= size) {
+    //             return create_block(current_heap, size);
+    //         } 
+    //         current_heap = current_heap->next;
+    //     }
 
-    printf("returning NULL\n");
-    return NULL;
+    //     t_heap *new_heap = NULL;
+    //     create_heap(&new_heap, allocation_size);
+    //     // Add the new heap to the end of the heap list
+    //     t_heap *last_heap = global_heap;
+    //     while (last_heap->next != NULL) {
+    //         last_heap = last_heap->next;
+    //     }
+    //     last_heap->next = new_heap;
+    //     // Return the newly allocated block
+    //     return create_block(new_heap, size);
+    // }
+
+    // printf("returning NULL\n");
+    // return NULL;
 };
 
 
