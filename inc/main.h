fd6dae08cf423452b88111d9d0b72e65239f7e8b
Mathius <info@yogimathius.dev>
2024-05-21 23:40:05 -0700

    feat: use main.h to include all other headers

---
+++ inc/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <sys/mman.h>
+
+#include "types.h"
+#include "hash_table.h"
+#include "pagemap.h"
+#include "my_malloc.h"
+#include "system_settings.h"
+#include "utils.h"
+#include "heap.h"
+#include "block.h"
+
+#endif 
