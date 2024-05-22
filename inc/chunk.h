20c551481e2e9e2a19d5643aa52b255cd7a3b747
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-22 16:17:59 -0700

    update: change parameters for create_tiny_chunk()

--- inc/chunk.h
+++ inc/chunk.h
@@ -5,7 +5,7 @@
 #include "types.h"
 
 t_chunk* create_chunk(t_page* page);
-t_tiny_chunk* create_tiny_chunk(t_page* page, size_t size);
+t_tiny_chunk* create_tiny_chunk(t_page* page, t_tiny_chunk* prev_chunk, size_t size);
 t_chunk* split_chunk(t_chunk* chunk, size_t size);
 
 #endif
