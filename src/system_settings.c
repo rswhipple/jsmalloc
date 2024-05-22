08d7df5549f6c5ef4ceb2d3732feda460fbeb79d
Mathius <info@yogimathius.dev>
2024-05-21 23:45:03 -0700

    chore: lint main.c and inc main.h, back up and running 🧨

--- src/system_settings.c
+++ src/system_settings.c
@@ -6,16 +6,11 @@ void system_settings() {
     if (pointer_size == 4) {
         min_chunk_size = 16;
         log_info("Minimum chunk size is 16 bytes (12 free).");
-        <<<<<< < HEAD
-    }
-    else {
-        ====== =
     }
     else {
         min_chunk_size = 24;
         log_info("Minimum chunk size is 24 bytes (20 free).");
-        >>>>>> > e7bf7f8(fix: add undefined check_system_size_t fn and include main.h)
-            size_t size_t_size = check_system_size_t();
+        size_t size_t_size = check_system_size_t();
         if (size_t_size == 4) {
             min_chunk_size = 24;
             log_info("Minimum chunk size is 24 bytes (20 free).");
