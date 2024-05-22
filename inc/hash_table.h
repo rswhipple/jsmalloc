190a889bcd32d4afee62bff3d20823800ef33ccd
Mathius <info@yogimathius.dev>
2024-05-21 23:38:49 -0700

    refactor: lint and remove includes in hash_table.h

--- inc/hash_table.h
+++ inc/hash_table.h
@@ -2,24 +2,16 @@
 #ifndef HASH_TABLE_H  
 #define HASH_TABLE_H
 
-#include "my_malloc.h"
-#include <stdio.h>
-#include <string.h>
-#include <stdint.h>
-#include <stdbool.h>
-
-#include <stdlib.h>
-#include <math.h>
-
+#include "types.h"
 
 // function prototypes
 unsigned int my_hash_function(size_t data_size, uint32_t table_size);
-t_hash *hash_table_create(t_pagemap *pagemap, uint32_t size, hash_function *hf) ;
-void hash_table_destroy(t_hash *ht);
-void hash_table_print(t_hash *ht);
+// t_hash *hash_table_create(t_heap *heap, uint32_t size, hash_function *hf) ;
+void hash_table_destroy(t_hash* ht);
+void hash_table_print(t_hash* ht);
 // bool hash_table_insert(t_heap **heap, t_hash *ht, const char *key, size_t value);
-void *hash_table_allocate(t_hash *ht, size_t key);
-void *hash_table_deallocate(t_chunk *chunk);
+void* hash_table_allocate(t_hash* ht, size_t key);
+// void *hash_table_deallocate(t_block *block);
 // void *hash_table_deallocate(t_hash *ht, const char *key);
 
 #endif
