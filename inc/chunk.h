7b3fe17047156ddf19bfa5068fe43281aaa56ba0
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-23 14:01:10 -0700

    add: create_top_chunk()

--- inc/chunk.h
+++ inc/chunk.h
@@ -4,6 +4,7 @@
 
 #include "types.h"
 
+t_chunk* create_top_chunk(t_page* page);
 t_chunk* create_chunk(t_page* page);
 t_tiny_chunk* create_tiny_chunk(t_page* page, t_tiny_chunk* prev_chunk, size_t size);
 t_chunk* split_chunk(t_chunk* chunk, size_t size);
