7c03439701faf19b4447ab14063b8d18d9b13b3d
Mathius <info@yogimathius.dev>
2024-05-23 22:54:24 -0700

    fix: stop creating pages once pages_left is 0

--- src/pagemap.c
+++ src/pagemap.c
@@ -1,6 +1,7 @@
 #include "../inc/main.h"
 
 void create_pageheap(t_pagemap** pagemap) {
+    log_info("creating pageheap");
     *pagemap = (t_pagemap*)mmap(0, BASE_HEAP_SIZE, PROT_READ |
                 PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0);
     (*pagemap)->span_head = create_base_span(*pagemap);
@@ -54,10 +55,8 @@ void create_fpages(t_pagemap* pagemap, t_span* span) {
     int chunk_size = min_chunk_size;
     while (page_count > 0 && chunk_size <= 64) {
         chunk_size += 8;
-        // how to align fpage with page?
-        // current->next = create_fpage(current, span, chunk_size);
-        // how to align fpage with page?
-        // current = current->next;
+        create_fpage(current, span, chunk_size);
+        current = (t_fpage*)current->base.next;
         page_count -= 1;
     }
 }
@@ -76,8 +75,8 @@ t_fpage* create_base_fpage(t_pagemap* pagemap, t_span* span) {
         fpage->base.memory = PAGE_SIZE - sizeof(t_span) - sizeof(t_fpage);
     }
     fpage->chunk_size = min_chunk_size;
-    // can we use create_top_tiny_chunk here?
-    // fpage->top_chunk = create_top_tiny_chunk(fpage);
+
+    create_top_tiny_chunk(fpage);
     return fpage;
 }
 
@@ -86,25 +85,26 @@ t_fpage* create_fpage(t_fpage* prev_page, t_span* span, int size) {
     UNUSED(size);
     t_fpage* page = (t_fpage*)MEMORY_SHIFT(FASTPAGE_SHIFT(prev_page), prev_page->base.memory);
     page->base.chunk_count = 1;
-    // how to align fpage with page?
-    // page->base.prev = prev_page;
+    page->base.prev = (t_base_page*)prev_page;
     page->base.next = NULL;
     page->base.memory = PAGE_SIZE - sizeof(t_fpage);
-    // can we use create_top_tiny_chunk here?
-    // page->base.top_chunk = create_top_tiny_chunk(page);
+    prev_page = page;
+    create_top_tiny_chunk(page);
     return page;
 }
 
 void create_pages(t_pagemap* pagemap, t_span* span) {
     // TODO: create and assign pages to types (fast, small, large)
-    int page_count = span->num_pages;
+    int pages_left = span->num_pages;
     t_page* current = NULL;
     span->page_head = create_base_page(pagemap, span);
-    page_count -= 1;
+    pages_left -= 1;
     current = span->page_head;
-    while (page_count < TINY_HEAP_ALLOCATION_SIZE) {
+    while (pages_left > 0) {
+        log_info("creating page");
+        printf("pages_left: %d\n", pages_left);
         current = create_page(current, span, fast);
-        page_count -= 1;
+        pages_left -= 1;
     }
 }
 
@@ -123,17 +123,17 @@ t_page* create_base_page(t_pagemap* pagemap, t_span* span) {
 t_page* create_page(t_page* prev_page, t_span* span, int pagetype) {
     UNUSED(span);
     t_page* page = (t_page*)MEMORY_SHIFT(PAGE_SHIFT(prev_page), prev_page->base.memory);
-    log_info("creating page");
-    printf("prev_page: %p\n", prev_page);
-    printf("page: %p\n", page);
+    // log_info("creating page");
+    // printf("prev_page: %p\n", prev_page);
+    // printf("page: %p\n", page);
     page->base.chunk_count = 1;
     page->base.prev = (t_base_page*)prev_page;
     page->base.next = NULL;
     page->base.memory = PAGE_SIZE - sizeof(t_page);
     page->pagetype = pagetype;
     if (pagetype == fast) {
-        log_info("creating top tiny chunk");
-        printf("page: %p\n", page);
+        // log_info("creating top tiny chunk");
+        // printf("page: %p\n", page);
         // TODO: can we use create_top_tiny_chunk here?
         create_top_tiny_chunk((t_fpage*)page);
     }
