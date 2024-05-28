1f26c21f39258901794e3182759171a17f94f0de
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-28 16:46:43 -0700

    fix: smaller min_chunk_size because of t_tiny_chunk data type

--- src/system_settings.c
+++ src/system_settings.c
@@ -5,20 +5,20 @@ void system_settings() {
     size_t pointer_size = check_system_pointer();
     if (pointer_size == 4) {
         min_chunk_size = 16;
-        log_info("Minimum chunk size is 16 bytes (12 free).");
+        log_info("Minimum chunk size is 8 bytes (4 free).");
     }
     else {
-        min_chunk_size = 24;
-        log_info("Minimum chunk size is 24 bytes (20 free).");
-        size_t size_t_size = check_system_size_t();
-        if (size_t_size == 4) {
-            min_chunk_size = 24;
-            log_info("Minimum chunk size is 24 bytes (20 free).");
-        }
-        else {
-            min_chunk_size = 24;
-            log_info("Minimum chunk size is 32 bytes (24 free).");
-        }
+        min_chunk_size = 16;
+        log_info("Minimum chunk size is 16 bytes (8 free).");
+        // size_t size_t_size = check_system_size_t();
+        // if (size_t_size == 4) {
+        //     min_chunk_size = 24;
+        //     log_info("Minimum chunk size is 24 bytes (20 free).");
+        // }
+        // else {
+        //     min_chunk_size = 24;
+        //     log_info("Minimum chunk size is 32 bytes (24 free).");
+        // }
     }
 }
 
