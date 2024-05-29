be62c4adfc695470bb4b0d7ecf814cf2f99990a6
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-28 21:23:20 -0700

    iterate: fpage function parameters

--- inc/pagemap.h
+++ inc/pagemap.h
@@ -21,9 +21,9 @@
 
 t_span* create_base_span(t_pagemap* pagemap);
 t_span* add_span(t_pagemap* pagemap, void* start, size_t size);
-void create_fpages(t_span* span);
-t_fpage* create_base_fpage(t_span* span);
-t_fpage* create_fpage(t_fpage* prev_page);
+void create_fpages(t_span* span, void* start);
+t_fpage* create_base_fpage(void* start);
+t_fpage* create_fpage(t_fpage* prev_page, size_t chunk_size);
 void create_pages(t_pagemap* pagemap, t_span* span);
 t_page* create_base_page(t_pagemap* pagemap, t_span* span);
 t_page* create_page(t_page* prev_page, t_span* span, int pagetype);
