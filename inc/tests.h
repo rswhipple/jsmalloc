f43d00ae9392633188d2fbdf7388a116848f0f6f
Mathius <info@yogimathius.dev>
2024-05-30 17:01:35 -0700

    chore: add cmocka tests.h and test file

---
+++ inc/tests.h
@@ -0,0 +1,12 @@
+#include <stdarg.h>
+#include <stddef.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+#ifndef TESTS_H_
+#define TESTS_H_
+
+void null_test_success(void** state);
+void int_test_success(void** state);
+
+#endif
