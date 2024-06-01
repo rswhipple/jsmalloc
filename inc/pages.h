1b4cbd6b47f8c395568478191e8aa3b40f37e158
Rebecca W Silverstein <rswhipple@gmail.com>
2024-05-31 21:54:26 -0700

    fix: fpage logic

--- inc/pages.h
+++ inc/pages.h
@@ -5,9 +5,10 @@
 
 #include "types.h"
 
-void create_fpages(t_span* span, void* start);
-t_fpage* create_base_fpage(void* start);
-t_fpage* create_fpage(t_fpage* prev_page, size_t chunk_size);
+void create_fpages(t_pagemap* pagemap);
+t_fpage* create_base_fpage(t_pagemap *pagemap);
+t_fpage* create_fpage(t_fpage* prev_page, int page_count, 
+        size_t chunk_size, t_cache* cache);
 void create_pages(t_pagemap* pagemap, t_span* span);
 t_page* create_base_page(t_pagemap* pagemap, t_span* span);
 t_page* create_page(t_page* prev_page, t_span* span, int pagetype);
