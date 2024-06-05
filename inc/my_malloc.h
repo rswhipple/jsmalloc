6240f25d2be69cfe5474e4c0601322a8693a6463
Rebecca W Silverstein <rswhipple@gmail.com>
2024-06-05 15:43:26 -0700

    add: search_heap(), get_size_category()

--- inc/my_malloc.h
+++ inc/my_malloc.h
@@ -8,5 +8,7 @@ void* my_malloc(size_t size);
 void my_free(void* ptr);
 void* my_realloc(void* ptr, size_t size);
 void* my_calloc(size_t nitems, size_t size);
+void* search_heap(size_t size, int category);
+int get_size_category(size_t size);
 
 #endif // MY_MALLOC_H
