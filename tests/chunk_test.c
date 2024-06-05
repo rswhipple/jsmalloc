fd275af77345d7857d56d7be8b5eea5868a3558c
Mathius <info@yogimathius.dev>
2024-06-04 17:04:45 -0700

    fix: update chunk to fd instead of next

--- tests/chunk_test.c
+++ tests/chunk_test.c
@@ -6,9 +6,10 @@ void chunk_test_success(void** state) {
   t_chunk* split = split_chunk(pagemap->span_head->page_head->top_chunk, 100);
   assert_int_equal(pagemap->span_head->page_head->top_chunk->size, 100);
   printf("split->size: %zu\n", split->size);
-  assert_int_equal((int*)split->next->size, 3836);
-  assert_int_equal(pagemap->span_head->page_head->top_chunk->next, split->next);
-  assert_int_equal(split->next->next, NULL);
-  assert_int_equal(pagemap->span_head->page_head->top_chunk->prev, NULL);
+  printf("split->fd->size: %zu\n", split->fd->size);
+  assert_int_equal((int*)split->fd->size, 3692);
+  assert_int_equal(pagemap->span_head->page_head->top_chunk->fd, split->fd);
+  assert_int_equal(split->fd->fd, NULL);
+  assert_int_equal(pagemap->span_head->page_head->top_chunk->bk, NULL);
   assert_int_equal(split, pagemap->span_head->page_head->top_chunk);
 }
