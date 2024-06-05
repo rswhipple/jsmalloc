0177703999b3f4e1a0452ad34fd6387d023a26bc
Rebecca W Silverstein <rswhipple@gmail.com>
2024-06-05 15:52:09 -0700

    rename: get_size_category() => get_page_type()

--- inc/my_malloc.h
+++ inc/my_malloc.h
@@ -9,6 +9,6 @@ void my_free(void* ptr);
 void* my_realloc(void* ptr, size_t size);
 void* my_calloc(size_t nitems, size_t size);
 void* search_heap(size_t size, int category);
-int get_size_category(size_t size);
+int get_page_type(size_t size);
 
 #endif // MY_MALLOC_H
--- src/my_malloc.c
+++ src/my_malloc.c
@@ -5,24 +5,24 @@ void* my_malloc(size_t size) {
         create_pagemap(&g_pagemap);
     }
 
-    // error check for negatives
+    // TODO error check for negatives
 
     // get category
-    int category = get_size_category(size);
+    int page_type = get_page_type(size);
 
     // search heap
-    void* ptr = search_heap(size, category);
+    void* ptr = search_heap(size, page_type);
     UNUSED(ptr);
 
     printf("returning NULL\n");
     return NULL;
 };
 
-void* search_heap(size_t size, int category) {
-    switch (category) {
+void* search_heap(size_t size, int page_type) {
+    switch (page_type) {
         case 1: return search_fast_cache(size);
-        case 2: return search_cache(size, category);
-        case 3: return search_cache(size, category);
+        case 2: return search_cache(size, page_type);
+        case 3: return search_cache(size, page_type);
         case 4: return allocate_huge_chunk(size);
         default: break;
     }
@@ -31,7 +31,7 @@ void* search_heap(size_t size, int category) {
     return NULL;
 }
 
-int get_size_category(size_t size) {
+int get_page_type(size_t size) {
     if (size < FAST_MAX_CHUNK_SIZE) return 1;
     else if (size < SMALL_MAX_CHUNK_SIZE) return 2;
     else if (size < LARGE_MAX_CHUNK_SIZE) return 3;
