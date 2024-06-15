24dbfd5c2d53278f2cbd2a67d696786ee09101cb
Rebecca W Silverstein <rswhipple@gmail.com>
2024-06-15 08:14:24 -0700

    update: chunk_test for desired behavior

--- tests/chunk_test.c
+++ tests/chunk_test.c
@@ -4,12 +4,11 @@ void chunk_test_success(void** state) {
   t_pagemap* pagemap = (t_pagemap*)*state;
 
   t_chunk* split = split_chunk(pagemap->span_head->page_head->top_chunk, 100);
-  printf("pagemap->span_head->page_head->top_chunk->size: %zu\n", CHUNK_SIZE(pagemap->span_head->page_head->top_chunk));
-  assert_int_equal(pagemap->span_head->page_head->top_chunk->size, 100);
-  printf("split->size: %zu\n", split->size);
-  printf("split->fd->size: %zu\n", split->fd->size);
+  printf("split->size: %zu\n", CHUNK_SIZE(split));
+  assert_int_equal(CHUNK_SIZE(split), 100);
+  assert_int_equal(CHUNK_SIZE(pagemap->span_head->page_head->top_chunk), CHUNK_SIZE(split));
   assert_int_equal(pagemap->span_head->page_head->top_chunk->fd, split->fd);
-  assert_int_equal(split->fd->fd, NULL);
+  assert_int_equal(split->fd, NULL);
   assert_int_equal(pagemap->span_head->page_head->top_chunk->bk, NULL);
   assert_int_equal(split, pagemap->span_head->page_head->top_chunk);
 }
