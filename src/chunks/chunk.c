c6bf159c384befbaf4d27a23806c797fcad62a72
Rebecca W Silverstein <rswhipple@gmail.com>
2024-06-19 20:16:58 -0700

    add: merge_chunks() and try_merge()

--- src/chunks/chunk.c
+++ src/chunks/chunk.c
@@ -45,12 +45,11 @@ t_chunk* split_chunk(t_chunk* chunk, size_t size) {
     second_chunk->fd = temp;
     write_boundary_tag(second_chunk);
 
-    // Add second_chunk to cache_table
-    char key[32];
-    snprintf(key, sizeof(key), "%zu", second_chunk->size);
-    cache_table_set(g_pagemap->frontend_cache->cache_table, key, second_chunk);
+    if (cache_table_set(second_chunk)) {    // add second_chunk to cache_table
+        fprintf(stderr, "Unable to add chunk to cache_table in split_chun()");
+        return NULL;
+    };
 
-    // Return the first chunk
     return first_chunk;
 }
 
@@ -65,15 +64,51 @@ t_chunk* allocate_huge_chunk(size_t size) {
     return huge_chunk;
 }
 
+static t_chunk* merge_chunks(t_chunk* value_1, t_chunk* value_2) {
+    // remove t_chunks from cache_table
+    if (value_1->bk) value_1->bk->fd = value_1->fd;
+    if (value_1->fd) value_1->fd->bk = value_1->bk;
+    if (value_2->bk) value_2->bk->fd = value_2->fd;
+    if (value_2->fd) value_2->fd->bk = value_2->bk;
+    printf("value_1->size before merge: %zu", value_1->size);
+    value_1->size += value_2->size;
+    printf("value_1->size after merge: %zu", value_1->size);
+    write_boundary_tag(value_1);
+
+    return value_1;
+}
+
+static t_chunk* try_merge(t_chunk* value) {
+    // coalescing algo
+    int flag = 0;
+    t_chunk* prev = PREV_CHUNK(value, PREV_SIZE(value));
+    t_chunk* next = NEXT_CHUNK(value); 
+
+    if (!IS_IN_USE(prev)) flag += 1;
+    if (!IS_IN_USE(next)) flag += 2;
+
+    switch (flag) {
+    case 1: return merge_chunks(prev, value);
+    case 2: return merge_chunks(value, next);
+    case 3: return merge_chunks(merge_chunks(prev, value), next);
+    default: break;
+    }
+
+    return value;
+}
+
 void free_chunk(void* ptr, size_t size) {
     UNUSED(size);
-    t_chunk* unsorted_chunk = g_pagemap->frontend_cache->unsorted_cache;
-    // check for other free chunks and write coalescing algo
+    t_chunk* value = (t_chunk*)((char*)ptr - sizeof(size_t));
+    value->bk = NULL;
+    value->fd = NULL;
+    SET_FREE(value);
+    value = try_merge(value);
 
-    // cast the memory space back into a chunk, set free and add to unsorted list
-    t_chunk* chunk = (t_chunk*)((char*)ptr - sizeof(size_t));
-    UNUSED(unsorted_chunk);
-    UNUSED(chunk);
+    // add to unsorted_cache
+    t_chunk* unsorted_chunk = g_pagemap->frontend_cache->unsorted_cache;
+    value->fd = unsorted_chunk;
+    g_pagemap->frontend_cache->unsorted_cache = value;
 }
 
 void free_huge_chunk(void* ptr, size_t size) {
