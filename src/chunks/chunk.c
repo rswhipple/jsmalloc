1c2ddf2b8260ed132589aa5e2411794376d028ab
Rebecca W Silverstein <rswhipple@gmail.com>
2024-06-21 11:54:53 -0700

    fix: spilt_chunk() logic, how to store second_chunk

--- src/chunks/chunk.c
+++ src/chunks/chunk.c
@@ -20,7 +20,7 @@ t_chunk* create_top_chunk(t_page* page) {
 
 // input parameters are t_chunk *chunk and size_t chunk_size
 t_chunk* split_chunk(t_chunk* chunk, size_t size) {
-    if (CHUNK_SIZE(chunk) <= size) {
+    if (CHUNK_SIZE(chunk) <= (size + 72)) {
         fprintf(stderr, "Invalid split size: %zu (chunk size: %zu)\n", size, chunk->size);
         custom_exit("Error in split_chunk()\n");
     }
@@ -29,7 +29,11 @@ t_chunk* split_chunk(t_chunk* chunk, size_t size) {
     t_chunk* temp = chunk->fd;
     size_t initial_chunk_size = chunk->size;
 
-    // WHAT HAPPENS TO THIS CHUNK? it is returned for use
+    int flag = 0;
+
+    if (g_pagemap->top_chunk == chunk) flag = 1;
+    else if (g_pagemap->last_chunk == chunk) flag = 2;
+
     // Update the original chunk's size, free status, next pointer & boundary_tag
     t_chunk* first_chunk = chunk;
     first_chunk->size = size;
@@ -46,13 +50,23 @@ t_chunk* split_chunk(t_chunk* chunk, size_t size) {
     second_chunk->fd = temp;
     write_boundary_tag(second_chunk);
 
-    if (cache_table_set(second_chunk))  // add second_chunk to cache_table
-        custom_exit("Unable to add chunk to cache_table in split_chunk()\n");
-
+    if (flag == 1) g_pagemap->top_chunk = second_chunk;
+    else if (flag == 2) g_pagemap->last_chunk = second_chunk;
+    else {
+        second_chunk->fd = g_pagemap->frontend_cache->unsorted_cache;
+        g_pagemap->frontend_cache->unsorted_cache = second_chunk;
+    }
+        
     return first_chunk;
 }
 
+
 t_chunk* allocate_huge_chunk(size_t size) {
+    if (size < (size_t)PAGE_SIZE) {
+        log_error("Error: allocation size too small.\n");
+        return NULL;
+    }
+
     // Allocate a huge chunk
     t_chunk* huge_chunk = (t_chunk*)mmap(NULL, size, PROT_READ | 
                 PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
@@ -72,24 +86,13 @@ t_chunk* merge_chunks(t_chunk* value_1, t_chunk* value_2) {
     if (value_2->bk) value_2->bk->fd = value_2->fd;
     if (value_2->fd) value_2->fd->bk = value_2->bk;
 
-    printf("value_1->size before merge: %zu\n", value_1->size);
     value_1->size += value_2->size;
-    printf("value_1->size after merge: %zu\n", value_1->size);
     write_boundary_tag(value_1);
 
     return value_1;
 }
 
 t_chunk* try_merge(t_chunk* value) {
-    if (IS_IN_USE(value)) {
-        printf("chunk is in use\n");
-        return value;
-    }
-    if (value->fd == NULL && value->bk == NULL) {
-        printf("chunk list is empty\n");
-        printf("value->size: %zu\n", value->size);
-        return value;
-    }
     // coalescing algo
     int flag = 0;
     printf("value->size: %zu\n", value->size);
@@ -98,14 +101,18 @@ t_chunk* try_merge(t_chunk* value) {
     printf("value->bk: %p\n", value->bk);
     printf("is_in_use: %lu\n", IS_IN_USE(value));
     t_chunk* prev = PREV_CHUNK(value, PREV_SIZE(value));
-    printf("prev->size: %zu\n", prev->size);
-    printf("prev pointer: %p\n", prev);
+    if (prev) {
+        printf("prev->size: %zu\n", prev->size);
+        printf("prev pointer: %p\n", prev);
+        if (!IS_IN_USE(prev)) flag += 1;
+    }
     t_chunk* next = NEXT_CHUNK(value);
-    printf("next->size: %zu\n", next->size);
-    printf("next pointer: %p\n", next);
+    if (next) {
+        if (!IS_IN_USE(next)) flag += 2;
+        printf("next->size: %zu\n", next->size);
+        printf("next pointer: %p\n", next);
+    }
 
-    if (!IS_IN_USE(prev)) flag += 1;
-    if (!IS_IN_USE(next)) flag += 2;
 
     switch (flag) {
     case 1: return merge_chunks(prev, value);
